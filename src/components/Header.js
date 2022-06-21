6efbccb9200863d188e7e856e750fbdf9d7f759c
edmcorrea <edm.correa@hotmail.com>
2022-06-21 14:52:42 -0300

    correcao de lint

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,13 @@ import React from "react";
 
 class Header extends React.Component {
   render() {
-    const { titulo } = this.props;
+    // const { titulo } = this.props;
     return (
       <header>
         {/* <h1>{titulo}</h1> */}
         <h1>Sistema Solar</h1>
       </header>
-    )
+    );
   }
 }
 
