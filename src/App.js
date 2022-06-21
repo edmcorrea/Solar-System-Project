2bd8b09af41cf1fd8c8f1a47d2e8d4f28b430560
edmcorrea <edm.correa@hotmail.com>
2022-06-21 14:39:25 -0300

    adicionando quesito 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import Header from './components/Header';
 
 class App extends React.Component {
   render() {
-    return (<p>Sistema Solar</p>);
+    return (
+      <Header />
+
+    // <p>Sistema Solar</p>
+    );
   }
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Header extends React.Component {
+  render() {
+    const { titulo } = this.props;
+    return (
+      <header>
+        {/* <h1>{titulo}</h1> */}
+        <h1>Sistema Solar</h1>
+      </header>
+    )
+  }
+}
+
+export default Header;
