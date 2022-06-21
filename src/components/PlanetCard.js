459c80eb07b96d98539b97597f165d8dafa571bb
edmcorrea <edm.correa@hotmail.com>
2022-06-21 15:55:31 -0300

    adicionando qcorrecoes de lint

--- src/components/PlanetCard.js
+++ src/components/PlanetCard.js
@@ -1,15 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class PlanetCard extends React.Component {
   render() {
-    const { planetName, planetImage } = this.props;
+    const { planetName } = this.props;
     return (
       <div data-testid="planet-card">
         <p data-testid="planet-name">{planetName}</p>
-        <img src={ planetImage } alt={ `Planeta ${planetName}` }></img>
+        {/* <img src={ planetImage } alt={ `Planeta ${planetName}` }></img> */}
       </div>
     );
   }
 }
+
+PlanetCard.propTypes = {
+  planetName: PropTypes.string.isRequired,
+  // planetImage: PropTypes.string.isRequired,
+};
 
 export default PlanetCard;
