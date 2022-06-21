a27b5e3b2a122ab8fa14f116f0314e3f233b7b09
edmcorrea <edm.correa@hotmail.com>
2022-06-21 16:56:11 -0300

    adicionando questios 8, 9 e 10

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
-import Missions from './data/missions';
+import Missions from './components/Missions';
 
 class App extends React.Component {
   render() {
